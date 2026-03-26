c7a32bbda695542c35db057bb8db82a3ef547645
lukino0737 <lukino0737@gmail.com>
2026-03-26 19:14:54 +0800

    随便写了点

---
+++ algorithm.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include"NavigationAPI.h"
+#include<vector>
+#include<string>
+
+PathResult calculate_shortest_path(int start_id, int end_id, bool consider_traffic)
+{
+    if(consider_traffic==false)
+    {
+        //算最短距离对应的路
+    }
+    else
+    {
+        //算考虑了交通情况从A到B的通行最短时间对应的路
+    }
+}
